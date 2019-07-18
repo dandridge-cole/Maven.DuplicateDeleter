2b5f68e1dd6b1fec8937b9675e7feea9beafb812
Dandridge Cole <dandridgecole@yahoo.com>
2019-07-18 08:55:59 -0400

    cleanup of access modifiers

--- src/main/java/com/zipcodewilmington/looplabs/DuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/DuplicateDeleter.java
@@ -6,10 +6,10 @@ import java.util.Arrays;
  * Created by leon on 1/25/18.
  */
 public abstract class DuplicateDeleter<T> implements DuplicateDeleterInterface<T> {
-    protected T[] array;
-    protected T[] uniques;
-    protected Integer countUniques;
-    protected Integer[] counts;
+    private T[] array;
+    private T[] uniques;
+    private Integer countUniques;
+    private Integer[] counts;
 
     public DuplicateDeleter(T[] intArray) {
         this.array = intArray;
@@ -25,9 +25,9 @@ public abstract class DuplicateDeleter<T> implements DuplicateDeleterInterface<T
         for(int i=0;i<this.countUniques;i++) if(this.counts[i]>=maxNumberOfDuplications)numToRemove+=this.counts[i];
         T[] newArray = Arrays.copyOf(this.array,this.array.length-numToRemove);
         Integer newCount=0;
-        for(int i=0; i<this.array.length;i++) {
-            if(this.counts[findIndex(this.array[i])]<maxNumberOfDuplications)
-                newArray[newCount++]=this.array[i];
+        for (T t : this.array) {
+            if (this.counts[findIndex(t)] < maxNumberOfDuplications)
+                newArray[newCount++] = t;
         }
         return newArray;
     }
@@ -37,31 +37,31 @@ public abstract class DuplicateDeleter<T> implements DuplicateDeleterInterface<T
         for(int i=0;i<this.countUniques;i++) if(this.counts[i]==exactNumberOfDuplications)numToRemove+=this.counts[i];
         T[] newArray = Arrays.copyOf(this.array,this.array.length-numToRemove);
         Integer newCount=0;
-        for(int i=0; i<this.array.length;i++) {
-            if(this.counts[findIndex(this.array[i])]!=exactNumberOfDuplications)
-                newArray[newCount++]=this.array[i];
+        for (T t : this.array) {
+            if (this.counts[findIndex(t)] != exactNumberOfDuplications)
+                newArray[newCount++] = t;
         }
         return newArray;
     }
 
-    public void makeUniqueList(){
-        for(int i=0;i<this.array.length;i++){
-            Integer checker = checkIfDuplicate(this.array[i]);
-            if(checker<0){
-                this.uniques[this.countUniques]=this.array[i];
-                this.counts[this.countUniques++]=1;
+    private void makeUniqueList(){
+        for (T t : this.array) {
+            Integer checker = checkIfDuplicate(t);
+            if (checker < 0) {
+                this.uniques[this.countUniques] = t;
+                this.counts[this.countUniques++] = 1;
             } else {
                 this.counts[checker]++;
             }
         }
     }
 
-    public Integer checkIfDuplicate(T toCheck){
+    private Integer checkIfDuplicate(T toCheck){
         for(int i=0;i<this.countUniques;i++)if(uniques[i].equals(toCheck))return i;
         return-1;
     }
 
-    public Integer findIndex(T toFind){
+    private Integer findIndex(T toFind){
         for(int i=0; i<this.countUniques;i++) if(this.uniques[i].equals(toFind))return i;
         return -1;
     }
